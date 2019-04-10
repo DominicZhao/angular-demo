852db9c6a6ad5160734b5b71f2aaac71e45c3344
DESKTOP-CUMDD9N\Zhao <zhaozemin@sinopatho.com>
2019-04-10 17:36:57 +0800

    user

--- taskmgr/src/app/services/user.service.ts
+++ taskmgr/src/app/services/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../domain';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -10,9 +12,15 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class UserService {
+  private readonly domain = 'users';
 
   constructor(
     private http: HttpClient,
     @Inject('BASE_CONFIG') private config,
   ) { }
+
+  searchUsers(filter: string) Observable<User[]> {
+
+  }
 }
+
